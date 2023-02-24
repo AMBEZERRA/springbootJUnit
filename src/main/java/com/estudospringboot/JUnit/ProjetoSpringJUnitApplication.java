a7451e4b4d0cca13bb905129835f2884413626f3
Allisson Bezerra <allisson.geografia@gmail.com>
2023-02-24 10:03:50 -0300

    First Commit - Projeto Criado

---
+++ src/main/java/com/estudospringboot/JUnit/ProjetoSpringJUnitApplication.java
@@ -0,0 +1,13 @@
+package com.estudospringboot.JUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringJUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringJUnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudospringboot/JUnit/ProjetoSpringJUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudospringboot.JUnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringJUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
