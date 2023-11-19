b78af754bef85e2b7e397f586de7d128d5277108
Allisson Bezerra <allisson.geografia@gmail.com>
2023-11-19 17:46:48 -0300

    Criando a entitade teste

--- src/main/java/com/estudospringboot/JUnit/ProjetoSpringJUnitApplication.java
+++ src/main/java/com/estudospringboot/JUnit/ProjetoSpringJUnitApplication.java
@@ -3,11 +3,13 @@ package com.estudospringboot.JUnit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.estudospringboot.JUnit.domain.User;
+
 @SpringBootApplication
 public class ProjetoSpringJUnitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringJUnitApplication.class, args);
-	}
-
+		}
+	
 }
